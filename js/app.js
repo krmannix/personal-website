bbc687bc197cb3b54b7b1fd32dd227eee7e78058
Kevin Mannix <kmannix@bu.edu>
2014-06-22 23:14:45 -0400

    Added fade-in & fade-out for lightbox

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 		var title = $(this).attr("title");
 		var alt = $(this).attr("alt");
 		$("body").append($background);
+		$background.hide();
 		$background.append($box);
 
 		// Put everything in the box
@@ -17,11 +18,16 @@ $(document).ready(function() {
 		$box.append('<img src="' + image + '" />');
 		$box.append('<p id="boxtext">' + alt + '</p>');
 
-		// Remove elements from box and destroy overlay
-		$(".overlay").click(function() {
-			$box.empty();
-			$(".overlay").remove();
+		$background.fadeIn("fast", function() {
+			$(".overlay").click(function() {
+				$background.fadeOut("fast", function() {
+					$box.empty();
+					$(".overlay").remove();
+				});
+			});
 		});
+		// Remove elements from box and destroy overlay
+		
 	});
 
 
