ade42b71fa8ca9a7e84467dc4ab9f7a80e2aa772
Kevin Mannix <kmannix@bu.edu>
2014-06-22 22:13:57 -0400

    Added overlay modal screen on click event. Removed dummy content.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
 
-	console.log("Just checking");
+	var $background = $('<div class="overlay"></div>');
+	var $box = $('<div class="infobox"></div>')
 
-	$(".gallery img").click(function() {
-		
+	$(".gallery img").click(function(event) {
+		event.preventDefault();
+		var image = $(this).attr("src");
+		var title = $(this).attr("title");
+		var alt = $(this).attr("alt");
+		$("body").append($background);
+		$background.append($box);
+
+		// Put everything in the box
+		console.log(title);
+		$box.append('<span id="boxtitle">' + title + '</span>');
+		$box.append('<img src="' + image + '" />');
+		$box.append('<p id="boxtext">' + alt + '</p>');
+
+		// Remove elements from box and destroy overlay
+		$(".overlay").click(function() {
+			$box.empty();
+			$(".overlay").remove();
+		});
 	});
+
 
 });
 
