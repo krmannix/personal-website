8dab7a98fa441de4f42e89fe11ac281cb1b49f3e
Kevin Mannix <kmannix@bu.edu>
2014-06-24 23:50:26 -0400

    Added SmartMart pproject and basic go button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 
 	var $background = $('<div class="overlay"></div>');
-	var $box = $('<div class="infobox"></div>')
+	var $box = $('<div class="infobox"></div>');
+	var $go_button = $('<button id="gobutton">Go!</button>');
 
 	$(".gallery img").click(function(event) {
 		event.preventDefault();
+		var link = $(this).parent().attr("href"); // Get "go link"
+		console.log(link);
 		var image = $(this).attr("src");
 		var title = $(this).attr("title");
 		var alt = $(this).attr("alt");
@@ -17,6 +20,12 @@ $(document).ready(function() {
 		$box.append('<span id="boxtitle">' + title + '</span>');
 		$box.append('<img src="' + image + '" />');
 		$box.append('<p id="boxtext">' + alt + '</p>');
+		if (typeof link != 'undefined') {
+			$go_button.click(function(event) {
+				window.location = link;
+			});
+			$box.append($go_button);
+		}
 
 		$background.fadeIn("fast", function() {
 			$(".overlay").click(function() {
