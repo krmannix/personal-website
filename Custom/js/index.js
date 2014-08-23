b6bceee8e09d5cbbb3813558a7ca4a036ea3f659
Kevin Mannix <kmannix@bu.edu>
2014-08-23 15:15:35 -0400

    Converted custom code to 3 seperate folders. Each folder has same content but different formatting through frameworks or improved custom code.

---
+++ Custom/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	/*var $profpic = $('#profilepic');
+	$profpic.animate({
+		height: "10px"
+	}, 
+	1000, 
+	function() {
+		console.log("Animation complete");
+	});
+*/
+});
